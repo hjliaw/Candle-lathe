654aba690b799df732da66681849bc589c3f3337
hjliaw <hawjyh@yahoo.com>
2023-10-18 21:09:37 -0700

    getting there

--- doc/grblgru_guide/smooth/gplot.py
+++ doc/grblgru_guide/smooth/gplot.py
@@ -19,7 +19,9 @@ from scipy.interpolate import UnivariateSpline
 
 xbs = 0.0      # x-axis backlash (z-axis is not critical for lathe)
 zmin = -1e6    # regoin to be smoothed
-zmax = -1e6 
+zmax = -1e6
+
+finPass = 1
 
 def usage(prog, exit_stat=0):
     str = 'Usage: %s [-x xbkls] [-r zmin:zmax] gcode\n' % prog
@@ -33,7 +35,6 @@ def usage(prog, exit_stat=0):
     else:              sys.stdout.write(str)
     sys.exit(exit_stat)
 
-
 def back2start(ltxt):
     # GrblGru diff versoins have different style of comments
     if re.findall( r'gcodebacktostart', ltxt) or \
@@ -46,7 +47,125 @@ def not_finish(ltxt):
     if re.findall( r'active =', ltxt, flags=re.IGNORECASE) :
         return True
     else:
-        return False 
+        return False
+
+def finish_passes(lines):
+    fin = []
+    nPass = 0
+    for line in Lines:
+        if re.findall( r'Finish', line): fin = [1]
+        if not_finish(line):             fin = []
+        if fin and back2start(line):     nPass += 1
+
+    print("finish pass(es) = %d" % nPass, file=sys.stderr )
+    return nPass
+    
+def smooth(lines, zrange):
+    comp = False
+    xold = 0.0
+    xnew = 0.0
+    zold = 0.0
+    znew = 0.0
+
+    smthdc = [[] for i in range(finPass+1)]
+    mx = [[] for i in range(finPass+1)]
+    mz = [[] for i in range(finPass+1)]
+
+    fin = []
+    finFR = [100]
+    finLines = [[] for i in range(finPass+1)]    # +1 to hold the final return gcodes
+    fp = 0
+
+    zmax = max(zrange)   # region to be smoothed
+    zmin = min(zrange)
+
+    for line in Lines:
+
+        if not_finish(line):  fin=[]
+
+        # backlash comp on retraction, as we should have "wind x in" when kissing the stock
+        comment = ""
+        if xpos := re.findall( r'[X](.?\d+.\d+)', line) :
+            xnew = float( xpos[0])
+            if (abs(xnew - xold) > 0.002 ):    # abs test to avoid rouding error from GrblGru
+                if ( not comp and xnew > xold ): comp = True
+                if (     comp and xnew < xold ): comp = False
+
+            if (comp and xbs > 1e-6):
+                xcomp = xnew + xbs
+                comment =  "     ; BKLS X%s -> X%.3f" % (xpos[0], xcomp)
+            else:
+                xcomp = xnew    
+
+            line = line.replace( 'X%s' % xpos[0], 'X%.3f' % xcomp )
+            xold = xnew
+
+        if zpos := re.findall( r'[Z](.?\d+.\d+)', line):
+            zold = znew
+            znew = float( zpos[0])
+
+        nline =  line.strip() + comment
+
+        if fin :      # process finish passes
+            if back2start(line): fp += 1
+            finLines[fp].append(nline)
+
+            if xpos and zpos and znew < zmax and znew > zmin :   # get (last) finish feedrate
+                finFR = re.findall( r'[F](.?\d+.\d+)', line)
+
+            if znew < zmax and znew > zmin :  # 2D array
+                mx[fp].append( xnew )
+                mz[fp].append( znew )
+        else:
+            print( nline )
+            fin = re.findall( r'( Finish )', line )
+
+    #----------------------------------------------------------------------------------------------
+
+    if finFR :
+        print("finish feedrate= %s" % finFR[0] , file=sys.stderr )
+    else:
+        print("finish feedrate not found" , file=sys.stderr )
+        sys.exit(1)
+
+    for fidx in range(finPass+1):
+        # spl wants increaseing 1st arg (conventionlly x, but z here)
+
+        mrz= mz[fidx][::-1]   # mz.reverse() NOT working     
+        mrx= mx[fidx][::-1]
+
+        if len(mrz) > 2 :
+            spl = UnivariateSpline(mrz, mrx )
+            zs = np.linspace( min(mz[fidx]), max(mz[fidx]), int( abs(zmax-zmin)*10) )    # every 0.1mm
+            xs = spl(zs)
+
+            if smthdc[fidx] :  smthdc[fidx].remove()
+            smthdc[fidx], = plt.plot( zs, xs, 'r-', lw=1)
+            plt.gcf().canvas.draw_idle()
+
+        # process the finish cut
+
+        for line in finLines[fidx]:
+            znew = 1e6
+            xpos = re.findall( r'[X](.?\d+.\d+)', line)
+            zpos = re.findall( r'[Z](.?\d+.\d+)', line)
+            if zpos: znew = float( zpos[0])
+
+            # asumption: z goes from 0 to negative dring finish
+            if znew >= zmax :
+                smoothed = False
+                print( line.strip() )
+            elif znew >= zmin :
+                if not smoothed :
+                    Nzs = len(zs)
+                    print("DBG: Nzs= %d" % Nzs , file=sys.stderr )
+                    print( "( smoothed curve begins )" )
+                    for i in range(Nzs,0,-1):  # N..1
+                        print( "F%s X%.3f Z%.3f" % (finFR[0], xs[i-1], zs[i-1]) )
+                    print( "( smoothed curve ends )" )
+                    smoothed = True
+            else:
+                print( line.strip() )
 
 #-----------------------------------------------------------------------------
 
@@ -92,22 +211,16 @@ fx=[]
 fz=[]
 
 fin=[]
-
 for line in Lines:
-
     if not_finish(line):  fin=[]
 
-    xpos = re.findall( r'[X](.?\d+.\d+)', line)
-    if xpos:
+    if xpos := re.findall( r'[X](.?\d+.\d+)', line) :
         xold = xnew
         xnew = float( xpos[0])
         
-    zpos = re.findall( r'[Z](.?\d+.\d+)', line)
-    if zpos:
+    if zpos := re.findall( r'[Z](.?\d+.\d+)', line) :
         zold = znew
         znew = float( zpos[0])
-
-    nline =  line.strip()
 
     if xpos or zpos :
         if fin :
@@ -124,22 +237,17 @@ for line in Lines:
 
 xmin = min( min(fx), min(rx) )
 xmax = max( max(fx), max(rx) )
-
 zmin = min( min(fz), min(rz) )
 zmax = max( max(fz), max(rz) )
-
-print(  'g-code limit : Z=[%.3f, %.3f]  X= [%.3f, %.3f]' % ( zmin, zmax, xmin, xmax) )
+print(  'g-code limit : Z=[%.3f, %.3f]  X= [%.3f, %.3f]' % ( zmin, zmax, xmin, xmax), file=sys.stderr  )
 
 zs = [None,None]
-
 bidx = 0
 bnd = [[], []]
 
 def mouse_event1(event):
-    #print(  'Z= %8.3f  X= %8.3f' % (event.xdata, event.ydata))
     global zs, bidx, bnd, plt
-
-    if event.button == 3 : # is MouseButton.RIGHT:  not defined
+    if event.button == 3 :   # is MouseButton.RIGHT:  not defined
         if bnd[bidx] : bnd[bidx].remove()
         bnd[bidx], = plt.plot( [event.xdata, event.xdata], [xmin, xmax], 'r-', lw=1)
         zs[bidx] = event.xdata    
@@ -148,8 +256,8 @@ def mouse_event1(event):
 
 def on_key(event):
     if event.key == 'S' :
-        print('smooth range Z = %.3f : %.3f' % (zs[0],  zs[1]))
-
+        print('smooth range Z = %.3f : %.3f' % (zs[0],  zs[1]), file=sys.stderr )
+        smooth( Lines, zs )
     
 fig = plt.figure()
 cid = fig.canvas.mpl_connect('button_press_event', mouse_event1)
