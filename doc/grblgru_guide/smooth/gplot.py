835810606383d939d28cb727cadde39916ccb773
hjliaw <hawjyh@yahoo.com>
2023-10-21 09:03:00 -0700

    gplot with simple gui

--- doc/grblgru_guide/smooth/gplot.py
+++ doc/grblgru_guide/smooth/gplot.py
@@ -17,6 +17,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
 
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
 xbs = 0.0      # x-axis backlash (z-axis is not critical for lathe)
 zmin = -1e6    # regoin to be smoothed
 zmax = -1e6
@@ -24,7 +28,7 @@ zmax = -1e6
 finPass = 1
 
 def dbg_print(msg):
-    print( msg, file=sys.stderr )
+    print( "    %s" % msg, file=sys.stderr )
 
 def usage(prog, exit_stat=0):
     str = 'Usage: %s [-x xbkls] [-r zmin:zmax] gcode\n' % prog
@@ -67,11 +71,28 @@ def finish_passes(lines):
         if fin and back2start(line): nPass += 1
     return nPass
 
+def increasing( a, b):  # ensure a is in increasing order
+    if len(a) < 2 : return a, b
+    ai = a
+    bi = b
+    if a[0] > a[1] :
+        ai = a[::-1]
+        bi = b[::-1]
+    return ai, bi
+
+def decreasing( a, b):
+    if len(a) < 2 : return a, b
+    ai = a
+    bi = b
+    if a[0] < a[1] :
+        ai = a[::-1]
+        bi = b[::-1]
+    return ai, bi
+
 smthdc   = [[] for i in range(20)]    # plt obj
 
-def smooth(lines, zrange):
+def smooth(lines, zrange, xrange, dir):
 
-    dbg_print("DBG: new file name = %s" % nfname )
     ngcfile = open( nfname, 'w')
     
     finPass = finish_passes(lines)
@@ -86,8 +107,11 @@ def smooth(lines, zrange):
     finFR = []           # (last) feedrate during finish cut
     fp = 0               # 0..finPass-1
 
-    zmax = max(zrange)   # in this function, z-region to be smoothed
-    zmin = min(zrange)
+    zmax = max(zrange) + 1e-4   # in this function, z-region to be smoothed
+    zmin = min(zrange) - 1e-4
+
+    xmax = max(xrange) + 1e-4   # in this function, z-region to be smoothed
+    xmin = min(xrange) - 1e-4
 
     comp = False
     xold = 0.0
@@ -129,8 +153,6 @@ def smooth(lines, zrange):
             
             if xpos and zpos and znew < zmax and znew > zmin :   # get (last) finish feedrate
                 finFR = re.findall( r'[F](.?\d+.\d+)', line)
-
-            if znew < zmax and znew > zmin :
                 mx[fp].append( xnew )
                 mz[fp].append( znew )
         else:
@@ -145,16 +167,29 @@ def smooth(lines, zrange):
         dbg_print("finish feedrate not found")
         sys.exit(1)
 
+    #----------------------------------------------------------------------------------------------
+            
+    # nearly vetical curve should be fit with z(x)
+    # while horizotal ones should be fit with x(z)   Q: can user specify this ?
+    
     for fidx in range(finPass+1):
         # spl wants increaseing 1st arg (conventionlly x, but is z here for lathe)
 
-        mrz= mz[fidx][::-1]   # .reverse() NOT working     
-        mrx= mx[fidx][::-1]
+        if dir == 'V':    # vertical fit lathe-X as the variable
+            mrx, mrz = increasing( mx[fidx], mz[fidx])
+        else :            # horizontal (only 'S' or 'H' will be passed here
+            mrz, mrx = increasing( mz[fidx], mx[fidx])
 
         if len(mrz) > 2 :
-            spl = UnivariateSpline(mrz, mrx )
-            zs = np.linspace( min(mz[fidx]), max(mz[fidx]), int( abs(zmax-zmin)*10) )    # every 0.1mm
-            xs = spl(zs)
+            # interpolate every 0.2mm, to be parameterized
+            if dir == 'V':
+                spl = UnivariateSpline(mrx, mrz )
+                xs = np.linspace( min(mx[fidx]), max(mx[fidx]), int( abs(xmax-xmin)*5) )    
+                zs = spl(xs)
+            else:  # normal horizontal curve fit
+                spl = UnivariateSpline(mrz, mrx )
+                zs = np.linspace( min(mz[fidx]), max(mz[fidx]), int( abs(zmax-zmin)*5) )
+                xs = spl(zs)
             
             # force end points to meet with shift (linearly scaled)
             # makes the transition smooth, but may be too close to rough cuts
@@ -164,13 +199,14 @@ def smooth(lines, zrange):
             for i in range(N):
                 xs[i] +=  s0 + (s1-s0)*i/(N-1)
             
-            # delete old if exists, add new
+            # delete old trace if exists, add new
             if smthdc[fidx] : smthdc[fidx].remove()   
             smthdc[fidx], = plt.plot( zs, xs, 'r.-', lw=1)
             plt.gcf().canvas.draw_idle()
 
         # process the finish cut
 
+        smoothed = False
         for line in finLines[fidx]:
             znew = 1e6
             xpos = re.findall( r'[X](.?\d+.\d+)', line)
@@ -178,16 +214,22 @@ def smooth(lines, zrange):
             if zpos: znew = float( zpos[0])
 
             # asumption: z goes from 0 to negative dring finish
-            if znew >= zmax :
-                smoothed = False
+
+            if znew >= zmax:
+                smoothed = False               # may not be set if zmax > 0
                 ngcfile.write( line + '\n' )
             elif znew >= zmin :
                 if not smoothed :
                     Nzs = len(zs)
                     dbg_print("DBG: Nzs= %d" % Nzs )
                     ngcfile.write( "( smoothed curve begins )\n" )
-                    for i in range(Nzs,0,-1):  # N..1
-                        ngcfile.write( "F%s X%.3f Z%.3f\n" % (finFR[0], xs[i-1], zs[i-1]) )
+                    # ensure z in decreasing order
+                    zs, xs = decreasing( zs, xs)
+                    for i in range(Nzs):
+                        ngcfile.write( "F%s X%.3f Z%.3f\n" % (finFR[0], xs[i], zs[i]) )
+
+                    #for i in range(Nzs,0,-1):  # N..1
+                    #    ngcfile.write( "F%s X%.3f Z%.3f\n" % (finFR[0], xs[i-1], zs[i-1]) )
                     ngcfile.write( "( smoothed curve ends )\n" )
                     smoothed = True
             else:
@@ -196,6 +238,7 @@ def smooth(lines, zrange):
     #----------------------------------------------------------------------------------------------
     
     ngcfile.close()
+    dbg_print("New gcode file %s geerated\n" % nfname )
 
 #-----------------------------------------------------------------------------
 
@@ -204,35 +247,47 @@ try:
 except:
     usage(sys.argv[0], 1)
     
-if len(args) == 0:
-    usage(sys.argv[0], 1)
+#if len(args) == 0:
+#    usage(sys.argv[0], 1)
 
 for opt, val in opts:
     if opt in ( '-h', '--help'):
         usage( sys.argv[0], 0 )
     elif opt in ('-x'):
         xbs = float(val)
-    elif opt in ('-r'):
-        rx = [-abs(float(i)) for i in val.split(":")]
-        zmax = max(rx)
-        zmin = min(rx)
 
-gcfile = open( args[0], 'r')
-Lines = gcfile.readlines()
-gcfile.close()
+root = tk.Tk()
+root.withdraw()
 
-if not re.findall( r'GrblGru', Lines[0]) :
-    dbg_print("ERROR: file %s is not a g-code generated by GrblGru" % args[0] )
-    sys.exit(2)
+while True:
+    global Lines, fnsel
+    fnsel = filedialog.askopenfilename( title="Select A G-Code file", \
+                                        filetypes=[("G-Code","*.gc"),("G-Code","*.ngc"),("G-Code","*.nc")])
 
-(fbase, fext) = args[0].split('.', 1)
+    if not fnsel: sys.exit(0)
+
+    gcfile = open( fnsel, 'r')
+    Lines = gcfile.readlines()
+    gcfile.close()
+
+    if re.findall( r'GrblGru', Lines[0]) :
+        break
+    else:
+        # tkinter generates ugly message box
+        msg = "File %s is not generated by GrblGru, continue ?" % fnsel
+        if messagebox.askyesno("Warning", msg ):
+            break
+        
+# allow long file name, or relative position
+
+idx = fnsel.rfind('.')
+fbase = fnsel[:idx]
+fext  = fnsel[idx+1:]
 
 nfname =  "%s_1.%s" % (fbase, fext)
 if underN := re.findall( r'_(\d+)$', fbase) :
     cnt = int(underN[0])
     nfname = "%s%d.%s" % (fbase[:-len(underN[0])], cnt+1, fext)
-
-#dbg_print( "new file name= %s" % nfname )
 
 #--------------------------------------------------------------------------------------------
 # figure out boundary box, generates data points for rough and finish cuts
@@ -279,24 +334,50 @@ zmin = min( min(fz), min(rz) )
 zmax = max( max(fz), max(rz) )
 dbg_print(  'g-code limit : Z=[%.3f, %.3f]  X= [%.3f, %.3f]' % ( zmin, zmax, xmin, xmax) )
 
-zs = [None,None]
+Zs = [None,None]
+Xs = [None,None]
 bidx = 0           # 0,1
 bnd = [[], []]     # plt objs of the two bounding lines
 
+def nearby(z, za):
+    d = 1e6
+    idx = -1
+    for i in range(len(za)):
+        if abs(z-za[i]) < d :
+            d = abs(z-za[i])
+            idx = i
+    if idx >= 0 :
+        return za[idx]
+    else:
+        return z
+        
 def mouse_event1(event):
-    global zs, bidx, bnd, plt
+    global Zs, Xs, bidx, bnd, plt
     if event.button == 3 :   # is MouseButton.RIGHT:  not defined
+        zn = nearby( event.xdata, fz )
+        xn = nearby( event.ydata, fx )
+        
+        # detect which end point
+        if Zs[0] and Zs[1] :
+            if abs(zn-Zs[0]) < 0.2*abs(zn-Zs[1]): bidx = 0
+            if abs(zn-Zs[1]) < 0.2*abs(zn-Zs[0]): bidx = 1
+        
         if bnd[bidx] : bnd[bidx].remove()
-        bnd[bidx], = plt.plot( [event.xdata, event.xdata], [xmin, xmax], 'r-', lw=1)
-        zs[bidx] = event.xdata    
+        bnd[bidx], = plt.plot( [zn, zn], [xmin, xmax], 'r-', lw=1)
+        Zs[bidx] = zn   #event.xdata
+        Xs[bidx] = xn   #event.ydata    
         bidx = 1 -bidx
         plt.gcf().canvas.draw_idle()
 
 def on_key(event):
-    if event.key == 'S' :
-        dbg_print('smooth range : Z=[%.3f, %.3f]' % (zs[0],  zs[1]) )
-        smooth( Lines, zs )
-    
+    if event.key == 'S' or event.key == 'H' or event.key == 'V' :
+        dbg_print('smooth range : Z=[%.3f, %.3f]' % (Zs[0],  Zs[1]) )
+        dbg_print('smooth range : X=[%.3f, %.3f]' % (Xs[0],  Xs[1]) )
+        smooth( Lines, Zs, Xs, event.key )
+
+    if event.key == 'q' or event.key == 'Q' :
+        sys.exit(0)
+        
 fig = plt.figure()
 cid = fig.canvas.mpl_connect('button_press_event', mouse_event1)
 cid = fig.canvas.mpl_connect('key_press_event', on_key)
